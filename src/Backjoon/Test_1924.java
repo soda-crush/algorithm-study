93d2abe7b13d798f446a4b8a03d05201c97e0648
soda-crush <idforsoda@gmail.com>
2021-08-30 16:13:31 +0900

    Add test 1924 solved

---
+++ src/Backjoon/Test_1924.java
@@ -0,0 +1,50 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test_1924 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = br.readLine().split(" ");
+
+        int month = Integer.parseInt(arr[0]);
+        int day = Integer.parseInt(arr[1]);
+
+        String monthStr = "";
+        String dayStr = "";
+
+        if(month < 10){
+            monthStr = "0" + month;
+        }else{
+            monthStr = Integer.toString(month);
+        }
+        if(day < 10){
+            dayStr = "0" + day;
+        }else{
+            dayStr = Integer.toString(day);
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+
+        Date newDay = sdf.parse("2007/01/01");
+        Date target = sdf.parse("2007/" + monthStr + "/" + dayStr);
+
+        // month, day - 1월 1일 = 날짜 계산
+
+        long diff = (target.getTime() - newDay.getTime())/(1000*60*60*24); // 초 - 분 - 시 - 일자수
+        int result = (int)diff%7;
+
+        if(result == 0) System.out.println("MON");
+        else if(result == 1) System.out.println("TUE");
+        else if(result == 2) System.out.println("WED");
+        else if(result == 3) System.out.println("THU");
+        else if(result == 4) System.out.println("FRI");
+        else if(result == 5) System.out.println("SAT");
+        else System.out.println("SUN");
+
+    }
+}
---
+++ src/Backjoon/Test_1924_2.java
@@ -0,0 +1,41 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Test_1924_2 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = br.readLine().split(" ");
+
+        int month = Integer.parseInt(arr[0]);
+        int day = Integer.parseInt(arr[1]);
+
+        int sum = 0;
+
+        int[] monthArr = {31, 28, 31, 30 ,31, 30, 31, 31, 30, 31, 30, 31};
+
+        for(int i = 0; i < month -1; i++){
+            sum += monthArr[i];
+        }
+        sum += day;
+
+        switch (sum % 7){
+            case 1 :
+                System.out.println("MON"); break;
+            case 2 :
+                System.out.println("TUE"); break;
+            case 3 :
+                System.out.println("WED"); break;
+            case 4 :
+                System.out.println("THU"); break;
+            case 5 :
+                System.out.println("FRI"); break;
+            case 6 :
+                System.out.println("SAT"); break;
+            case 0 :
+                System.out.println("SUN"); break;
+        }
+    }
+}
