a84c82182510cad90457e15fb619cee3e1c2307b
soda-crush <idforsoda@gmail.com>
2021-09-09 13:56:59 +0900

    Add test 2442 solved

---
+++ src/Backjoon/Test_2442.java
@@ -0,0 +1,29 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Test_2442 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int max = 2 * n - 1;
+        int blank = max/2;
+        int star = 1;
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < blank; j++){
+                sb.append(" ");
+            }
+            blank--;
+            for(int j = 0; j < star; j++){
+                sb.append("*");
+            }
+            star = star + 2;
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
