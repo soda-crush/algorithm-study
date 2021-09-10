dc2d9d40b845b4d7ffa5b9aa16656aa943e5e34f
soda-crush <idforsoda@gmail.com>
2021-09-10 12:21:40 +0900

    Add test 10991 solved

---
+++ src/Backjoon/Test_10991.java
@@ -0,0 +1,25 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Test_10991 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int k = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - k; j++){
+                sb.append(" ");
+            }
+            for(int j = 1; j < 2 * k; j++){
+                if(j%2 == 0) sb.append(" ");
+                else sb.append("*");
+            }
+            sb.append("\n");
+            k++;
+        }
+        System.out.println(sb.toString());
+    }
+}
