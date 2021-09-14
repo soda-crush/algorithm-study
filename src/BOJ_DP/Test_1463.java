3360b3daa2e544ef29ec4741acea8eb2572250c2
soda-crush <idforsoda@gmail.com>
2021-09-14 10:42:56 +0900

    Add test 1463 solved

---
+++ src/BOJ_DP/Test_1463.java
@@ -0,0 +1,33 @@
+package BOJ_DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Test_1463 {
+    static Integer[] dp;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        dp = new Integer[n + 1];
+        dp[0] = dp[1] = 0;
+
+        System.out.println(recur(n));
+    }
+
+    static int recur(int n){
+        if(dp[n] == null){
+            if(n % 6 == 0){
+                dp[n] = Math.min(Math.min(recur(n / 2), recur(n / 3)), recur(n - 1)) + 1;
+            }else if(n % 3 == 0){
+                dp[n] = Math.min(recur(n / 3), recur(n - 1)) + 1;
+            }else if(n % 2 == 0){
+                dp[n] = Math.min(recur(n / 2), recur(n - 1)) + 1;
+            }else{
+                dp[n] = recur(n - 1) + 1;
+            }
+        }
+
+        return dp[n];
+    }
+}
